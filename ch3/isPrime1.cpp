0866521da58db09d481674d0dc44efdbe580eb63
Mehedee Hassan <Mehedee Hassan>
2016-07-31 13:01:36 +0600

    ch3

--- ch3/isPrime1.cpp
+++ ch3/isPrime1.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
 
+using namespace std;
+
+
+bool isPrime (int n){
+
+ if(n <=1) return 0;
+
+ int limit = sqrt(n);
+ for( int i = 2 ; i <= limit  ; i++){
+    if(n % i)
+        return false;
+ }
+
+
+ return true;
+}
+
+
+
+int main(){
+
+
+
+    if(isPrime(10)){
+        printf("prime");
+    }else{
+        cout<<"not prime";
+    }
+
+    return 0;
+
+
+}
