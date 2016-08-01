d8fbc5ed1b1a30a744b7af81559f846f41d29183
Mehedee Hassan <Mehedee Hassan>
2016-08-02 00:29:44 +0600

    data structure

---
+++ ch5/linked_list1.cpp
@@ -0,0 +1,67 @@
+#include<cstdio>
+#include<iostream>
+#include<cmath>
+#include<vector>
+
+
+using namespace std;
+
+struct node{
+    int roll;
+    node *next;
+};
+
+node *root = NULL;
+
+
+void append(int roll){
+
+    if(root == NULL){
+        root=new node();
+        root->roll = roll;
+        root->next = NULL;
+
+    }else{
+
+        node *current_node = root;
+        while(current_node->next != NULL){
+            current_node = current_node->next;
+        }
+        node *newnode = new node();
+        newnode->roll = roll;
+        newnode->next = NULL;
+
+
+    current_node->next = newnode;
+    }
+
+}
+
+
+
+
+void print(){
+
+    node *currentNode = root;
+
+
+    while(currentNode != NULL){
+
+        cout<<currentNode->roll<<endl;
+
+        if(currentNode->next == NULL)
+            break;
+
+        currentNode = currentNode->next;
+    }
+
+}
+
+int main(){
+
+    append(2);
+    append(3);
+    append(4);
+
+return 0;
+}
